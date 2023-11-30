54a81b44de675af1af283580504ab280a4ee3c59
Gagandeep singh <120259381+gagandeepsinghsodhi@users.noreply.github.com>
2023-11-30 22:27:15 +0530

    Add files via upload

---
+++ Exp.1_type.py
@@ -0,0 +1,31 @@
+
+a=5
+print("Type of a:",type(a))
+b=5.0
+print("Type of b:",type(b))
+c=2+4j
+print("Type of c:",type(c))
+d="Welcome to CSE Department"
+print("Type of d:",type(d))
+e=["Python","Programming","Language"]
+print("Type of e:",type(e))
+f=("Miet","CSE","Student")
+print("Type of f:",type(f))
+g=range(10)
+print("Type of g:",type(g))
+h={"name":"Dhruv","age":21}
+print("Type of h:",type(h))
+i={"Data","Types","Python"}
+print("Type of i:",type(i))
+j=frozenset({"Object","Oriented","Programming"})
+print("Type of j:",type(j))
+k=True
+print("Type of k:",type(k))
+l=b"Python"
+print("Type of l:",type(l))
+m=bytearray(4)
+print("Type of m:",type(m))
+n=memoryview(bytes(6))
+print("Type of n:",type(n))
+o=None
+print("Type of o:",type(o))
